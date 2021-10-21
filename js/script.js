d1baaa9a9b9ffdc5452166c6cee4b18d9e39fd26
mdeias <mdeias@icloud.com>
2021-10-21 16:38:00 +0200

    prezzo e sconto

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@
 
 const km = prompt( "Quanti km vuoi percorrere?");
 const anni = prompt( "Quanti anni hai?");
+
+let prezzo = (0.21 * km);
+
+const sconto18 = (0.042 * km);
+const sconto65 = (0.084 * km);
+
+console.log(prezzo);
+
+if (anni < 18){
+
+    console.log(prezzo - sconto18);
+}
+
+if (anni > 65){
+
+    console.log(prezzo - sconto65);
+}
