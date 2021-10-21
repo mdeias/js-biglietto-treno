c0408511a7dc16d01d6c5e4c6ed59564c6d53d42
mdeias <mdeias@icloud.com>
2021-10-21 16:15:32 +0200

    Variabili utente

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// definisco le variabili da chiedere all'utente (km, età)
+// definisco variabile prezzo 
+// inserisco sconti 
+// stampo il prezzo
+
+const km = prompt( "Quanti km vuoi percorrere?");
+const anni = prompt( "Quanti anni hai?");
