6f24afbd55bb08732d254ce0e2c1d3cb4d622dc7
mdeias <mdeias@icloud.com>
2021-10-21 18:40:02 +0200

    correzione stampa con sconto

--- js/script.js
+++ js/script.js
@@ -12,17 +12,23 @@ let prezzo = (0.21 * km).toFixed(2);
 const sconto18 = (0.042 * km);
 const sconto65 = (0.084 * km);
 
+const prezzo18 = (prezzo - sconto18).toFixed(2);
+const prezzo65 = (prezzo - sconto65).toFixed(2);
+
 console.log(prezzo);
+document.getElementById ("costo").innerHTML = (prezzo) + "€";
 
 if (anni < 18){
 
-    console.log(prezzo - sconto18);
+    console.log(prezzo18);
+    document.getElementById ("costo").innerHTML = (prezzo18) + "€";
 }
 
 if (anni > 65){
 
-    console.log(prezzo - sconto65);
+    console.log(prezzo65);
+    document.getElementById ("costo").innerHTML = (prezzo65) + "€";
 }
 
 
-document.getElementById ("prezzo").innerHTML = prezzo + "€";
+
